4b8d1e369f791e742a71f33c51323f5fdcb30530
gxk <liang.lu@duitang.com>
2018-12-17 18:48:05 +0800

    :bug: fix: ls cmd

--- src/main/java/com/gxk/demo/cmd/ListCmd.java
+++ src/main/java/com/gxk/demo/cmd/ListCmd.java
@@ -5,7 +5,6 @@ import com.gxk.demo.core.Env;
 import com.gxk.demo.core.EnvHolder;
 import com.gxk.demo.logger.ILog;
 import com.gxk.demo.logger.LogFactory;
-import com.gxk.demo.util.CmdUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,10 +22,19 @@ public class ListCmd implements CmdHandler {
   private static final Logger log = LoggerFactory.getLogger(CmdHandler.class);
   private static final ILog fj = LogFactory.getLog();
 
+
   @Override
   public void apply(String... args) {
-    if (CmdUtils.isHelpCmd(args)) {
-      fj.info("help ....");
+    if (args.length == 1 && Arrays.asList("-h", "--help").contains(args[0])) {
+      fj.error("fj ls [repo]");
+      return;
+    }
+
+    boolean showAll = true;
+    String repo = null;
+    if (args.length > 0) {
+      repo = args[0];
+      showAll = false;
     }
 
     // get global config
@@ -38,26 +46,39 @@ public class ListCmd implements CmdHandler {
     }
 
     Map<String, String> sc = (Map<String, String>) val;
-    sc.forEach((key, vp) -> {
-      fj.error("Scaffolds Group: " + key);
 
-      Path path = Paths.get(vp);
-      File file = path.toFile();
-      if (!file.exists()) {
-        fj.info("Nothing.");
+    if (!showAll) {
+      if (!sc.containsKey(repo)) {
+        fj.error("not found repo, " + repo);
         return;
       }
 
-      try {
-        Files.list(path).map(it -> it.toFile().getName())
-          .forEach(it -> {
-            fj.info("Scaffold: " + it);
-          });
-      } catch (IOException e) {
-        log.error("io err", e);
-      }
+      String path = sc.get(repo);
+      ListCmd.accept(repo, path);
+      return;
+    }
+    sc.forEach(ListCmd::accept);
+  }
 
-      fj.info("\n-----------------\n");
-    });
+  private static void accept(String key, String vp) {
+    fj.error("Scaffolds Group: " + key);
+
+    Path path = Paths.get(vp);
+    File file = path.toFile();
+    if (!file.exists()) {
+      fj.info("Nothing.");
+      return;
+    }
+
+    try {
+      Files.list(path).map(it -> it.toFile().getName())
+        .forEach(it -> {
+          fj.info("Scaffold: " + it);
+        });
+    } catch (IOException e) {
+      log.error("io err", e);
+    }
+
+    fj.info("\n-----------------\n");
   }
 }
