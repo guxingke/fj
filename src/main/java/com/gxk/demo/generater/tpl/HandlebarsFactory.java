d1d1929ddc2f9871d0514a79f6176f8f402885e2
gxk <liang.lu@duitang.com>
2019-03-26 13:41:38 +0800

    :sparkles: .

--- src/main/java/com/gxk/demo/generater/tpl/HandlebarsFactory.java
+++ src/main/java/com/gxk/demo/generater/tpl/HandlebarsFactory.java
@@ -29,6 +29,14 @@ public class HandlebarsFactory {
           cs[0] -= 32;
           return String.valueOf(cs);
         });
+
+    handlebars.registerHelper("capitalize",
+        (con, options) -> {
+          char[] cs = ((String) con).toCharArray();
+          cs[0] -= 32;
+          return String.valueOf(cs);
+        });
+
     // java package name , 下划线,中横线去除 , special char remove -> sc_rm
     handlebars.registerHelper("sc_rm",
       (con, options) -> con.toString().replaceAll("-|_", ""));
